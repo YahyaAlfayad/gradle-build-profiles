0be6518cb5bc7119422d744a437d04c97b41d5d8
yahyaalfayad <yahya.alfayad@gmail.com>
2016-10-06 00:41:31 +0200

    run tests script added

---
+++ runTests.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+SCRIPT_PID=$$
+
+function checkExitStatus {
+
+    if [[ $1 != 0 ]]
+    then
+        echo "exit status does not indicate success. failed at stage $2"
+        cd ..
+        kill -KILL $SCRIPT_PID
+    fi
+}
+
+cd build-profiles-plugin
+
+../gradlew clean uploadArchives $@
+
+checkExitStatus $? "uploading plugin binaries"
+
+cd ../gradle-profiles-plugin-consumer
+
+../gradlew createProfiles $@
+
+checkExitStatus $? "creating consumer profiles"
+
+./gradlew build $@
+
+checkExitStatus $? "building consumer"
